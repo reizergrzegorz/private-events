69b3e8c7076b1f694eeb7562920024401666b8e5
reizergrzegorz <reizergrzegorz@gmail.com>
2025-03-27 13:20:40 +0100

    refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-  root "events#index"
   devise_for :users
   devise_scope :user do
     get "users/sign_out" => "users/sessions#destroy"
   end
+  root "events#index"
   resources :events
-  resources :users
+  resources :users, only: [:show]
 end
