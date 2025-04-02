070403240025238a6d4ce24cf6e70017a1385118
reizergrzegorz <reizergrzegorz@gmail.com>
2025-04-02 17:47:37 +0200

    add upcoming and past events to user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @created_events = Event.where(creator_id: @user.id).all
-        @attended_events = @user.attended_events.all
+        @past_events = @user.attended_events.past
+        @upcoming_events = @user.attended_events.upcoming
     end
 
 end
